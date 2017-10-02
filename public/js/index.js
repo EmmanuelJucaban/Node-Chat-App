2561a1702d7c6274bb21ac083e3eaf44af9e4d84
Emmanuel <emmanueljucaban@gmail.com>
2017-10-02 00:46:43 -0700

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 // The code here is for client connecting to the server
 var socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+  // heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev() .innerHeight();
+
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('Connected to server');
 });
@@ -19,6 +36,7 @@ socket.on('newMessage', function(message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -29,7 +47,7 @@ socket.on('newLocationMessage', function(message) {
     url: message.url,
     createdAt: formattedTime
   });
-  
+
   $('#messages').append(html);
 });
 
